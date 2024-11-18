070c3a2be40790af78d9a4b3223c8b5313dc16e6
thx-to <tlsekghk@gmail.com>
2024-11-18 12:36:03 +0900

    Local Updated

--- HTML_241113_02_PRTC.js
+++ HTML_241113_02_PRTC.js
@@ -9,3 +9,25 @@ btnNavToggle.addEventListener("click", function () {
 
 
 
+      const text = "CONTACT@BATCREW.CO.KR";
+      const container = document.getElementById("rollingText");
+      const numRepeats = 6; // 문구 반복 횟수 설정
+
+      // 문구를 반복해서 추가하는 함수
+      function createRollingText() {
+        for (let i = 0; i < numRepeats; i++) {
+          const span = document.createElement("span");
+          span.textContent = text;
+          container.appendChild(span);
+        }
+
+        // 문구가 반복 끝날 때 간격이 생기지 않도록 첫 번째 문구 다시 추가
+        for (let i = 0; i < numRepeats; i++) {
+          const span = document.createElement("span");
+          span.textContent = text;
+          container.appendChild(span);
+        }
+      }
+
+      // 페이지 로드 후 문구 생성
+      createRollingText();
